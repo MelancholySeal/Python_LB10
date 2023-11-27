f39b6c6dbe803012ecbfb80bb60967f86ece4b2e
MelancholySeal <kirillazimoff@yandex.ru>
2023-11-27 20:05:27 +0300

    deleted comms

--- Project10/tasks/gentask1.py
+++ Project10/tasks/gentask1.py
@@ -2,14 +2,10 @@
 # -*- coding: utf-8 -*-
 if __name__ == "__main__":
 
-    # Определение множества гласных букв
     vowels = {'a', 'e', 'i', 'o', 'u', 'y'}
 
-    # Ввод строки с клавиатуры
     input_string = input("Введите строку: ")
 
-    # Подсчет количества гласных в строке
     count_vowels = sum(1 for char in input_string.lower() if char in vowels)
 
-    # Вывод результата
     print(f"Количество гласных в введенной строке: {count_vowels}")
